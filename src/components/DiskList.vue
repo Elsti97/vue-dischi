12dbcf9b285d274b0e7056eefa31cf443411e3e0
Elsti97 <elsti.shehati@hotmail.it>
2022-11-29 22:55:18 +0100

    Exercise done with computed

--- src/components/DiskList.vue
+++ src/components/DiskList.vue
@@ -1,49 +1,70 @@
 <template>
   <div>
-    <div class="d-flex flex-wrap rows mt-5 justify-content-center">
-      <CardComp  v-for="(element, index) in AlbumCop" :key="index" :Album="element"  />
+    <div class="d-flex flex-wrap rows mt-3 justify-content-center">
+      <CardComp
+        v-for="(element, index) in funzioneComputed"
+        :key="index"
+        :Album="element"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import CardComp from './CardComp.vue';
-import axios from 'axios'
+import CardComp from "./CardComp.vue";
+import axios from "axios";
 
-  export default {
-    name: "DiskList",
-    components: {
-      CardComp,
-    },
-  data(){
-    return{
+export default {
+  name: "DiskList",
+  components: {
+    CardComp,
+  },
+  props: {
+    propsScelta: String,
+  },
+  data() {
+    return {
       AlbumCop: [],
       arrayGeneri: [],
-    }
+    };
   },
-  mounted(){
+  computed: {
+    funzioneComputed() {
+      if (this.propsScelta == "") {
+        return this.AlbumCop;
+      } else {
+        return this.AlbumCop.filter((element) => {
+          return element.genre == this.propsScelta;
+        });
+      }
+    },
+  },
+  mounted() {
     this.getAlbum();
   },
   methods: {
-    getAlbum(){
-      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-      .then((response) =>{
-        this.AlbumCop = response.data.response;
+    getAlbum() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((response) => {
+          this.AlbumCop = response.data.response;
 
-        this.AlbumCop.forEach((singoloAlbum)=>{
-          if(!this.arrayGeneri.includes(singoloAlbum.genre)){
-            this.arrayGeneri.push(singoloAlbum.genre)
-          }
-        })
-      });
+          this.AlbumCop.forEach((singoloAlbum) => {
+            if (!this.arrayGeneri.includes(singoloAlbum.genre)) {
+              this.arrayGeneri.push(singoloAlbum.genre);
+            }
+          });
+
+          this.$emit("emitGeneri", this.arrayGeneri);
+        });
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.rows{
-    width: 60%;
-    margin: auto;
-  }
+.rows {
+  width: 60%;
+  margin: auto;
+}
 </style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <select v-model="valueOption">
-      <option :value="elem" v-for="(elem, index) in arrayGeneri" :key="index">{{elem}}</option>
-    </select>
+    <div class="mt-5">
+      <select v-model="valueOption">
+        <option value="">All genres</option>
+        <option :value="element" v-for="(element, index) in arrayGeneri" :key="index">{{element}}</option>
+      </select>
+    </div>
+    
     <div>
-      <DiskList />
+      <DiskList :propsScelta="valueOption" @emitGeneri="methodsGeneri" />
     </div>
   </div>
-
 </template>
 
 <script>
@@ -18,17 +21,22 @@ export default {
   components: {
     DiskList,
   },
-  props: {
-      arrayGeneri: String,
-    },
   data(){
-      return{
-        valueOption: '',
-      }
+    return{
+      valueOption: '',
+      arrayGeneri: [],
+    }
   },
+  methods: {
+      methodsGeneri(valoreEmit){
+        this.arrayGeneri = valoreEmit
+      }
+    }
 };
 </script>
 
 <style lang="scss" scoped>
-
+  select{
+    width: 10%;
+  }
 </style>
